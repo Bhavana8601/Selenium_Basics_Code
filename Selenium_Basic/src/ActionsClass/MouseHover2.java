e5339992e8dc561a167383dd66a66d03775e6913
bhavana ahire <bhavanaahire8601@gmail.com>
2025-05-26 16:16:44 +0530

    changes in action package

--- Selenium_Basic/src/ActionsClass/MouseHover2.java
+++ Selenium_Basic/src/ActionsClass/MouseHover2.java
@@ -1,8 +1,12 @@
 package ActionsClass;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class MouseHover2 {
 	
@@ -12,7 +16,23 @@ public class MouseHover2 {
 	driver.manage().window().maximize();
 	driver.get("https://amazon.in/");
 	
-	driver.findElement(By.id("nav-link-accountList")).click();
+	//driver.findElement(By.id("nav-link-accountList")).click();
+	Actions action = new Actions(driver);
+	action.moveToElement(driver.findElement(By.xpath("//div[@id='nav-link-accountList']"))).perform();
+	
+	
+	
+	List<WebElement> yourLists = driver.findElements(By.xpath("//div[@id='nav-al-wishlist']/ul/li/a/span"));
+	for(WebElement ele: yourLists)
+	{
+		System.out.println(ele.getText());
+		if(ele.getText().equals("Discover Your Style"))
+		{
+			ele.click();
+			break;
+		}
+	}
+	
 	
 	}
 
